a1701d967f83a4f3acdf45bb39b5185989da187a
DANIEL TEMITAYO PETER <tayodaniel0@gmail.com>
2025-04-28 01:34:42 +0100

    Changes to be committed: 	modified:   investment-cal/src/app/app.component.html 	modified:   investment-cal/src/app/app.component.ts 	modified:   investment-cal/src/app/header/header.component.html 	new file:   investment-cal/src/app/interface/interface.component.css 	new file:   investment-cal/src/app/interface/interface.component.html 	new file:   investment-cal/src/app/interface/interface.component.ts

--- investment-cal/src/app/app.component.ts
+++ investment-cal/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { InterfaceComponent } from './interface/interface.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, InterfaceComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ investment-cal/src/app/interface/interface.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-interface',
+  imports: [],
+  templateUrl: './interface.component.html',
+  styleUrl: './interface.component.css'
+})
+export class InterfaceComponent {
+
+}
